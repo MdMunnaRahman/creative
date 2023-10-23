332a3388a6eed2b085f0be4a9da4969e177faf10
MdMunnaRahman <146363445+MdMunnaRahman@users.noreply.github.com>
2023-10-23 12:20:10 +0600

    crative my project

---
+++ script/main.js
@@ -0,0 +1,36 @@
+// Swper js
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+// Swper Js end
+
+
+// Counter js
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+	entries.forEach( entry => {
+		const el = entry.target
+		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+			counterUp( el, {
+				duration: 2000,
+				delay: 16,
+			} )
+			el.classList.add( 'is-visible' )
+		}
+	} )
+}
+
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+const el = document.querySelector( '.counter' )
+IO.observe( el )
+
+// AOS Js
+AOS.init();
